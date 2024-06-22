d120e2eb5b4886e564b1c110a00cf994e1a450cc
Aash <31497659+AashPau@users.noreply.github.com>
2024-06-22 20:38:36 +1000

    fix the responsiveness of top bar for mobile devices

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,10 +1,10 @@
 export const Top = () => {
   return (
-    <div className="min-h-32 flex p-5 z-20 start-0 border-b bg-black items-center fixed top-0 w-full justify-around poppins-regular box-border">
-      <h1 className="text-transparent bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text ms-2 font-extrabold text-5xl">
+    <div className="min-h-32 md:flex p-5 z-20 start-0 border-b bg-black items-center fixed top-0 w-full justify-around poppins-regular box-border">
+      <h1 className="text-transparent bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text ms-2 font-extrabold text-5xl mb-4 md:mb-0 text-center md:text-start">
         Aashish Paudel
       </h1>
-      <div className="flex ms-auto gap-3 text-white items-center me-5">
+      <div className="flex md:ms-auto gap-3 text-white items-center justify-around md:me-5 box-border">
         <div>HOME</div>
         <div>SKILLS</div>
         <div>ABOUT</div>
