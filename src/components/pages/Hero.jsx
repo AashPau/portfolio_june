ed313d5af4da14b6f640be87b767e9f155c449d3
Aash <31497659+AashPau@users.noreply.github.com>
2024-06-22 20:26:52 +1000

    making Hero section responsive for smaller devices

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -6,36 +6,38 @@ import { FaLinkedin } from "react-icons/fa";
 
 export const Hero = () => {
   return (
-    <div className="bg-black min-h-dvh flex poppins-regular">
-      <ParticlesComp className="-z-10" />
-      <div className=" ms-20 flex items-center gap-5 z-10">
-        <div className="ps-4 flex flex-col w-1/2 ">
-          <div className="flex flex-col info p-4 text-white me-auto gap-2 ">
-            <h2 className="text-4xl">
-              Hi, I am <span className=" text-white">Aashish.</span>
-            </h2>
-            <div className="text-3xl poppins-extrabold inline-block">
-              I'm a <TypingEffect />
-            </div>
-            <div>
-              <h2 className=" text-2xl">
-                Energetic and Enthuastic Full stack Mern Developer with great
-                passion for programming.
+    <div>
+      <div className="bg-black min-h-dvh flex poppins-regular">
+        <ParticlesComp className="-z-10" />
+        <div className=" md:ms-20 flex items-center gap-5 z-10 box-border">
+          <div className="ps-4 flex flex-col md:w-1/2 ">
+            <div className="flex flex-col info p-4 text-white  gap-2 ">
+              <h2 className="text-4xl">
+                Hi, I am <span className=" text-white">Aashish.</span>
               </h2>
+              <div className="text-3xl poppins-extrabold inline-block box-border">
+                <TypingEffect />
+              </div>
+              <div>
+                <h2 className=" text-2xl">
+                  Energetic and Enthuastic Full stack Mern Developer with great
+                  passion for programming.
+                </h2>
+              </div>
+            </div>
+            <div className="flex gap-4 p-4 text-white">
+              <button className="bg-blue-600 p-3 rounded">DOWNLOAD CV</button>
+              <button className=" border-blue-500 border-2 rounded-full">
+                <FaGithub className="text-5xl" />
+              </button>
+              <button className=" border-blue-500 border-2 ">
+                <FaLinkedin className="text-5xl" />
+              </button>
             </div>
           </div>
-          <div className="flex gap-4 p-4 text-white">
-            <button className="bg-blue-600 p-3 rounded">DOWNLOAD CV</button>
-            <button className=" border-blue-500 border-2 rounded-full">
-              <FaGithub className="text-5xl" />
-            </button>
-            <button className=" border-blue-500 border-2 ">
-              <FaLinkedin className="text-5xl" />
-            </button>
+          <div className=" items-start w-1/3 hidden md:block">
+            <Image />
           </div>
-        </div>
-        <div className="flex items-start w-1/3 ">
-          <Image />
         </div>
       </div>
     </div>
--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,6 +1,6 @@
 export const Top = () => {
   return (
-    <div className="min-h-32 flex p-5 bg-black items-center fixed top-0 w-full justify-around poppins-regular">
+    <div className="min-h-32 flex p-5 z-20 start-0 border-b bg-black items-center fixed top-0 w-full justify-around poppins-regular box-border">
       <h1 className="text-transparent bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text ms-2 font-extrabold text-5xl">
         Aashish Paudel
       </h1>
