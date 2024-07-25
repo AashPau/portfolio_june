4717673cde807564fc25b17c06a7bbb47de76f5c
Aash <31497659+AashPau@users.noreply.github.com>
2024-07-25 19:20:09 +1000

    fixed the skills section

--- src/components/pages/MySkills.jsx
+++ src/components/pages/MySkills.jsx
@@ -8,12 +8,17 @@ const skills = [bootstrapLogo, javascriptLogo, reactLogo, htmlLogo, cssLogo];
 
 export const MySkills = () => {
   return (
-    <div className="bg-black text-white">
-      <h1>My Skills</h1>
-      <div className="flex gap-3 ">
-        {skills.map((skill, i) => (
-          <img key={i} src={skill} />
-        ))}
+    <div className="poppins-regular relative p-4 bg-black text-white z-30 box-border">
+      <div className=" bg-slate-500 m-10 rounded py-4">
+        <div className="flex justify-center">
+          <h1 className="text-3xl ms-4 mb-2 font-bold">My Skills</h1>
+        </div>
+        <hr />
+        <div className="flex justify-around gap-5 m-4">
+          {skills.map((skill, i) => (
+            <img key={i} src={skill} width="80" className="rounded" />
+          ))}
+        </div>
       </div>
     </div>
   );
